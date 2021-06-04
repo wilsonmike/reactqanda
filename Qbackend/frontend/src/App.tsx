c0435514e17adbaaa77390e028251f2a4b69808e
wilsonmike <michaelwilsonsj@hotmail.com>
2021-06-04 11:27:58 -0400

    added homepage component

--- Qbackend/frontend/src/App.tsx
+++ Qbackend/frontend/src/App.tsx
@@ -1,12 +1,13 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 import { Header } from './Header';
+import { HomePage } from './HomePage';
 
 function App() {
   return (
     <div className="App">
       <Header />
+      <HomePage />
     </div>
   );
 }
---
+++ Qbackend/frontend/src/HomePage.tsx
@@ -0,0 +1,8 @@
+export const HomePage = () => (
+  <div>
+    <div>
+      <h2>Unanswered Questions</h2>
+      <button>Ask a question</button>
+    </div>
+  </div>
+);
