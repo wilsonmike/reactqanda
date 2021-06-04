4b883a5b03bd190ea93b66dd081092c89c10a3d6
wilsonmike <michaelwilsonsj@hotmail.com>
2021-06-04 12:44:47 -0400

    unanswered questions stored on homepage

--- Qbackend/frontend/src/HomePage.tsx
+++ Qbackend/frontend/src/HomePage.tsx
@@ -1,8 +1,12 @@
+import { QuestionList } from './QuestionList';
+import { getUnansweredQuestions } from './QuestionsData';
+
 export const HomePage = () => (
   <div>
     <div>
       <h2>Unanswered Questions</h2>
       <button>Ask a question</button>
     </div>
+    <QuestionList data={getUnansweredQuestions()} />
   </div>
 );
