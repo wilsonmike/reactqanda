3a8dce0ad658a438c5f6b001e45545b09e2df9dd
wilsonmike <michaelwilsonsj@hotmail.com>
2021-06-04 11:22:05 -0400

    added user profile svg

--- Qbackend/frontend/src/Header.tsx
+++ Qbackend/frontend/src/Header.tsx
@@ -4,5 +4,8 @@ export const Header = () => (
   <div>
     <a href="./">Q & A</a>
     <input type="text" placeholder="Search..." />
+    <a href="./signin">
+      <span>Sign In</span>
+    </a>
   </div>
 );
---
+++ Qbackend/frontend/src/Icons.tsx
@@ -0,0 +1,4 @@
+import React from 'react';
+import user from './profile-user.svg';
+
+export const UserIcon = () => <img src={user} alt="User" width="12px" />;
